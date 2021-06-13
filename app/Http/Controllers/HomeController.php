77a9f7a751e8b0437143bf7fbd8a664f98208cac
eric9178 <eric9178vadim@gmail.com>
2021-06-14 03:37:14 +0900

    fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
-    public function getInvited(Request $request){
-        print_r($request->all());exit;
+    public function getInvited(Request $request)
+    {
+        $emailcontent = [];
+        Mail::send('emails.getinvited', $emailcontent, function ($message) {
+            $message->to('eric9178vadim@gmail.com', 'Learning Laravel Support')
+                ->subject('Contact using Our Contact Form');
+        });
     }
 }
