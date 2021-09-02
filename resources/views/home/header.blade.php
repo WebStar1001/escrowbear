0c808c956bafc213dae77e937c18494b201aacfb
eric9178 <eric9178vadim@gmail.com>
2021-09-02 18:17:24 +1000

    fixed

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,3 +1,31 @@
+<div class="container-fluid">
+    <div class="nk-header-wrap">
+        <div class="flow-text">
+            <div class="info-banner-text">Global Market Cap : <span class="text-success">$2,587,436,874</span></div>
+            <div class="info-banner-text">BTC Dominance : <span class="text-success">BTC: 42.2% ETH: 18.7%</span></div>
+            <div class="info-banner-text">24hr Volume : <span class="text-success">$273,838,238,220</span></div>
+            <div class="info-banner-text">EscrowBear©Market : <span class="text-success">$3,585,663</span></div>
+        </div>
+        <div class="nk-header-tools">
+            <ul class="nk-quick-nav">
+                <li class="dropdown notification-dropdown mr-n1">
+                    <a href="#" class="dropdown-toggle nk-quick-nav-icon" data-toggle="dropdown">
+                        <div class="dropdown-indicator user-name">Location</div>
+                    </a>
+
+                    <div class="dropdown-menu">
+                        <ul class="link-list-opt">
+                            <li><a href="#"><span>Malaysia</span></a></li>
+                            <li><a href="#"><span>Russia</span></a></li>
+                            <li><a href="#"><span>Unite State</span></a></li>
+                            <li><a href="#"><span>United Kingdom</span></a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 <header>
     <div class="logo">EscrowBear</div>
     <div class="menu">
