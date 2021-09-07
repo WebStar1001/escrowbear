9ed849f942accf4f3544e626b32849cd08205e9e
eric9178 <eric9178vadim@gmail.com>
2021-09-08 01:38:20 +1000

    fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,112 +22,119 @@
 <div class="nk-app-root">
     <!-- main @s -->
     <div class="nk-main ">
-        <div class="nk-split nk-split-page nk-split-md">
+        <div class="nk-wrap nk-wrap-nosidebar">
             <!-- content @s -->
-            <div class="nk-split-content nk-block-area nk-block-area-column nk-auth-container bg-white">
+            <div class="nk-content login-screen">
                 <div class="nk-block nk-block-middle nk-auth-body  wide-xs">
-                    <div class="brand-logo pb-4 text-center">
+                    <div class="brand-logo pb-3 text-center">
                         <a href="{{route('home')}}" class="logo-link">
-                            <h2 class="logo">Escrowbear</h2>
+                            <img class="logo" src="{{asset_url('assets/img/home/mark.png')}}"/>
                         </a>
+                    </div>
+                    <div class="brand-logo pb-3 text-center">
+                        <h3 class="title">Welcome to EscrowBear</h3>
+                    </div>
+                    <div class="brand-logo pb-2 text-center">
+                        <p class="sub-title">Login using your email address</p>
                     </div>
                     <div class="card card-bordered">
                         <div class="card-inner card-inner-lg">
-                            <div class="nk-block-head">
-                                <div class="nk-block-head-content">
-                                    <h4 class="nk-block-title">Sign-In</h4>
-                                    <div class="nk-block-des">
-                                        <p>Access the CryptoLite panel using your email and passcode.</p>
-                                    </div>
-                                </div>
-                            </div>
                             <form action="{{route('login')}}" method="post">
                                 @csrf
                                 <div class="form-group">
                                     <div class="form-label-group">
-                                        <label class="form-label" for="default-01">Email or Username</label>
+                                        <label class="form-label" for="default-01">Email Address</label>
                                     </div>
                                     <input type="text" class="form-control form-control-lg" id="email" name="email"
-                                           placeholder="Enter your email address or username">
+                                           placeholder="Enter your email address or username" required>
 
                                 </div>
                                 <div class="form-group">
-                                    <div class="form-label-group">
-                                        <label class="form-label" for="password">Passcode</label>
-                                        <a class="link link-primary link-sm" href="html/pages/auths/auth-reset-v2.html">Forgot
-                                            Code?</a>
-                                    </div>
-                                    <div class="form-control-wrap">
-                                        <a href="#" class="form-icon form-icon-right passcode-switch"
-                                           data-target="password">
-                                            <em class="passcode-icon icon-show icon ni ni-eye"></em>
-                                            <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
-                                        </a>
-                                        <input type="password" class="form-control form-control-lg" id="password"
-                                               name="password" placeholder="Enter your passcode">
-                                        @if($errors->has('email'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first() }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
+                                    {{--                                    <div class="form-label-group">--}}
+                                    {{--                                        <label class="form-label" for="password">Passcode</label>--}}
+                                    {{--                                        <a class="link link-primary link-sm" href="html/pages/auths/auth-reset-v2.html">Forgot--}}
+                                    {{--                                            Code?</a>--}}
+                                    {{--                                    </div>--}}
+                                    {{--                                    <div class="form-control-wrap">--}}
+                                    {{--                                        <a href="#" class="form-icon form-icon-right passcode-switch"--}}
+                                    {{--                                           data-target="password">--}}
+                                    {{--                                            <em class="passcode-icon icon-show icon ni ni-eye"></em>--}}
+                                    {{--                                            <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>--}}
+                                    {{--                                        </a>--}}
+                                    {{--                                        <input type="password" class="form-control form-control-lg" id="password"--}}
+                                    {{--                                               name="password" placeholder="Enter your passcode">--}}
+                                    {{--                                        @if($errors->has('email'))--}}
+                                    {{--                                            <span class="help-block">--}}
+                                    {{--                                                <strong>{{ $errors->first() }}</strong>--}}
+                                    {{--                                            </span>--}}
+                                    {{--                                        @endif--}}
+                                    {{--                                    </div>--}}
                                 </div>
                                 <div class="form-group">
-                                    <button class="btn btn-lg btn-primary btn-block">Sign in</button>
+                                    <button class="btn btn-lg btn-primary btn-block">Continue</button>
                                 </div>
                             </form>
                         </div>
                     </div>
+                    <div class="brand-logo pt-1 text-center">
+                        <p class="sub-title">Don't you have EscrowBear Account? <a href="#">Get Invited</a></p>
+                    </div>
+                    <div class="brand-logo pt-2 text-center">
+                        <p class="sub-description">Please check that you are visiting the correct URL</p>
+                    </div>
+                    <div class="brand-logo pt-2 text-center">
+                        <span class="sub-url">
+                            <em class="ni ni-lock-fill"></em> https://escrowbear.com/login
+                        </span>
+                    </div>
+                    <div class="footerDiv">
+                        <div class="justify-content-center text-center detail-1">Earth's only HyperEscrow
+                            <a href="#">Log in <em class="icon ni ni-arrow-to-right"></em> </a>
+                        </div>
+                        <ul class="nav nav-sm justify-content-center langDiv pt-2">
+                            <li class="nav-item dropup active current-page">
+                                <a class="dropdown-toggle dropdown-indicator has-indicator nav-link"
+                                   data-toggle="dropdown" data-offset="0,10"><span>English</span></a>
+                                <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                                    <ul class="language-list">
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="{{asset_url('dashlite/images/flags/english.png')}}" alt=""
+                                                     class="language-flag">
+                                                <span class="language-name">English</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="{{asset_url('dashlite/images/flags/spanish.png')}}" alt=""
+                                                     class="language-flag">
+                                                <span class="language-name">Español</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="{{asset_url('dashlite/images/flags/french.png')}}" alt=""
+                                                     class="language-flag">
+                                                <span class="language-name">Français</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="language-item">
+                                                <img src="{{asset_url('dashlite/images/flags/turkey.png')}}" alt=""
+                                                     class="language-flag">
+                                                <span class="language-name">Türkçe</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item"> <a class="nav-link">version 1.3</a></li>
+                            <li class="nav-item"> <a href="#" class="nav-link">FAQ</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div
-                class="nk-split-content nk-split-stretch bg-lighter d-flex toggle-break-lg toggle-slide toggle-slide-right"
-                data-content="athPromo" data-toggle-screen="lg" data-toggle-overlay="true">
-                <div class="slider-wrap w-100 w-max-550px p-3 p-sm-5 m-auto">
-                    <div class="slider-init" data-slick='{"dots":true, "arrows":false}'>
-                        <div class="slider-item">
-                            <div class="nk-feature nk-feature-center">
-                                <div class="nk-feature-img">
-                                    <img class="round" src="{{asset_url('dashlite/images/slides/promo-a.png')}}" srcset="
-                                         {{asset_url('dashlite//images/slides/promo-a2x.png')}} 2x" alt="">
-                                </div>
-                                <div class="nk-feature-content py-4 p-sm-5">
-                                    <h4>Escrowbear</h4>
-                                    <p>You can start to create your products easily with its user-friendly design & most
-                                        completed responsive layout.</p>
-                                </div>
-                            </div>
-                        </div><!-- .slider-item -->
-                        <div class="slider-item">
-                            <div class="nk-feature nk-feature-center">
-                                <div class="nk-feature-img">
-                                    <img class="round" src="{{asset_url('dashlite/images/slides/promo-b.png')}}"
-                                         srcset="{{asset_url('dashlite/images/slides/promo-b2x.png')}} 2x" alt="">
-                                </div>
-                                <div class="nk-feature-content py-4 p-sm-5">
-                                    <h4>Escrowbear</h4>
-                                    <p>You can start to create your products easily with its user-friendly design & most
-                                        completed responsive layout.</p>
-                                </div>
-                            </div>
-                        </div><!-- .slider-item -->
-                        <div class="slider-item">
-                            <div class="nk-feature nk-feature-center">
-                                <div class="nk-feature-img">
-                                    <img class="round" src="{{asset_url('dashlite/images/slides/promo-c.png')}}"
-                                         srcset="{{asset_url('dashlite/images/slides/promo-c2x.png')}} 2x" alt="">
-                                </div>
-                                <div class="nk-feature-content py-4 p-sm-5">
-                                    <h4>Escrowbear</h4>
-                                    <p>You can start to create your products easily with its user-friendly design & most
-                                        completed responsive layout.</p>
-                                </div>
-                            </div>
-                        </div><!-- .slider-item -->
-                    </div><!-- .slider-init -->
-                    <div class="slider-dots"></div>
-                    <div class="slider-arrows"></div>
-                </div><!-- .slider-wrap -->
+
+
             </div>
             <!-- wrap @e -->
         </div>
