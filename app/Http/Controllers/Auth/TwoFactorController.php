45d7a4625d2fb37745844f94a33a0fc491d83e44
eric9178 <eric9178vadim@gmail.com>
2021-09-10 14:37:17 +1000

    fixed

--- app/Http/Controllers/Auth/TwoFactorController.php
+++ app/Http/Controllers/Auth/TwoFactorController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class TwoFactorController extends Controller
 {
